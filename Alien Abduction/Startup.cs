ed64d8faa8c3caa2c7c444edc04b8ad322454a1b
rinesaavdyli <66117589+rinesaavdyli@users.noreply.github.com>
2022-01-19 19:54:16 +0100

    Mongodb setup

--- Alien Abduction/Startup.cs
+++ Alien Abduction/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
 
 namespace Alien_Abduction
 {
@@ -25,12 +26,22 @@ namespace Alien_Abduction
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c=>
+            {
+                c.AddPolicy("AllowOrigin", options=> options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            });
+            services.AddControllersWithViews().AddNewtonsoftJson(options =>
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+                = new DefaultContractResolver());
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
