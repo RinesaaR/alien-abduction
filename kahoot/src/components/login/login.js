2fa1add498ae52fc4122b733fc5b700a1cf7ebb8
Lidra Kamberi <lidra@asist.space>
2022-01-26 22:11:55 +0100

    Login and Register Form Validation

--- kahoot/src/components/login/login.js
+++ kahoot/src/components/login/login.js
@@ -2,11 +2,43 @@ import React from "react";
 import kahoot from "../../kahoot.png";
 
 export class Login extends React.Component {
+  state ={ 
+    username: "",
+    password: "",
+    errors: {},
+  }
+
+  onChange = (e) => {
+    this.setState({[e.target.name] : e.target.value});
+  }
+
+  formValidation = () => {
+    const {username, email, password} = this.state;
+    let isValid = true;
+    const errors = {};
+
+    if (username.trim().length <5){
+      errors.usernameLength = "Username must be of Length 5 or higher!";
+      isValid = false;
+    }
+    if (password.trim().length <8){
+      errors.passwordLength = "Password must be of Length 8 or higher!";
+      isValid = false;
+    }
+    this.setState({errors});
+    return isValid;
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const isValid = this.formValidation();
+  }
   constructor(props) {
     super(props);
   }
 
   render() {
+    const{username, password, errors} = this.state;
     return (
       <div className="base-container" ref={this.props.containerRef}>
         <div className="header">Log in</div>
@@ -18,18 +50,21 @@ export class Login extends React.Component {
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username" value={username} onChange={this.onChange} />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password" value={password} onChange={this.onChange} />
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="button" className="btn">
+          <button type="button" className="btn" onClick={this.onSubmit}>
             Login
           </button>
+          {Object.keys(errors).map((key)=>{
+              return <div style={{color: 'red'}} key={key} >{errors[key]}</div>
+          })}
         </div>
       </div>
     );
