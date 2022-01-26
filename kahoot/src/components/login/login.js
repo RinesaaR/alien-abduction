5354e4b129d7b2a0a2623c13f65cf2ca405f06d6
Lidra Kamberi <lidra@asist.space>
2022-01-26 17:04:56 +0100

    Login & Register Frontend

---
+++ kahoot/src/components/login/login.js
@@ -0,0 +1,37 @@
+import React from "react";
+import kahoot from "../../kahoot.png";
+
+export class Login extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="base-container" ref={this.props.containerRef}>
+        <div className="header">Log in</div>
+        
+        <div className="content">
+          <div className="image">
+            <img src={kahoot} />
+          </div>
+          <div className="form">
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="username" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="password" />
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <button type="button" className="btn">
+            Login
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
