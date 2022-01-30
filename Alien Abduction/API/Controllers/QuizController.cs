65b67bca4da7df5a031eda442ca5baa7108fb797
rinesaavdyli <66117589+rinesaavdyli@users.noreply.github.com>
2022-01-30 11:42:51 +0100

    Creating Quiz Crud using CQRS

--- Alien Abduction/API/Controllers/QuizController.cs
+++ Alien Abduction/API/Controllers/QuizController.cs
@@ -3,30 +3,43 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using DatabaseLogic;
+using Application.Quizzes;
+using System.Threading;
 
 namespace API.Controllers
 {
     public class QuizController : BaseApiController
     {
-        private readonly DataContext context;
-
-        public QuizController(DataContext context)
-        {
-            this.context = context;
-        }
+        
 
         [HttpGet]
         public async Task<ActionResult<List<Quiz>>> GetQuizzes()
         {
-            return await this.context.Quizzes.ToListAsync();
+            return await this.Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Quiz>> GetQuiz(Guid id)
         {
-            return await this.context.Quizzes.FindAsync(id);
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> CreateQuiz(Quiz quiz)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Quiz = quiz }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditQuiz(Guid id, Quiz quiz)
+        {
+            quiz.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Quiz = quiz }));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteQuiz(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command { Id = id }));
         }
     }
 }
--- Alien Abduction/API/Startup.cs
+++ Alien Abduction/API/Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using DatabaseLogic;
+using MediatR;
+using Application.Quizzes;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -30,14 +34,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(this.config.GetConnectionString("DefaultConnection"));
-            });
+            services.AddApplicationServices(config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
